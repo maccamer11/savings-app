2b1b9af86ca0cfdbb85b7bb52b53765067a66790
Das <maccameron@live.ca>
2020-01-29 16:25:09 -0700

    Updates

---
+++ precache-manifest.2d7e6c6acdbb3836e22bd9813fa58fb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506f733009e7288192ba4d476aef9a23",
+    "url": "/savings-app/index.html"
+  },
+  {
+    "revision": "e92a9feeefcc1ada343a",
+    "url": "/savings-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b64856e669f636bb7f66",
+    "url": "/savings-app/static/css/main.c4f20d7d.chunk.css"
+  },
+  {
+    "revision": "e92a9feeefcc1ada343a",
+    "url": "/savings-app/static/js/2.4071f12a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/savings-app/static/js/2.4071f12a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b64856e669f636bb7f66",
+    "url": "/savings-app/static/js/main.c07401cf.chunk.js"
+  },
+  {
+    "revision": "a7520e14647f79bb89cc",
+    "url": "/savings-app/static/js/runtime-main.bd2bc4ca.js"
+  },
+  {
+    "revision": "e06fded0812e179e0d1519b01b8d5720",
+    "url": "/savings-app/static/media/background.e06fded0.svg"
+  }
+]);
